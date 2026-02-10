988ca234c60e25946b715933e4a07e9cc28070df
Rifat-Bin-Zakir <rifat135.me@gmail.com>
2026-02-10 09:53:46 -0500

    Create imaging (runs fine) previous edit by Jack - for capturing calibration scale images.py

---
+++ src/imaging (runs fine) previous edit by Jack - for capturing calibration scale images.py
@@ -0,0 +1,57 @@
+import os
+from imaging import infinity_cam
+import cv2
+import datetime
+from stage import Stage
+
+
+class infinity_cam():
+    def __init__(self, device_id=0, flip=True):
+        try:
+            self.cam = cv2.VideoCapture(device_id)
+            self.cam.set(cv2.CAP_PROP_FRAME_WIDTH, 1616)
+            self.cam.set(cv2.CAP_PROP_FRAME_HEIGHT, 1216)
+            self.cam.set(cv2.CAP_PROP_BUFFERSIZE, 1)
+            self.flip = flip
+        except Exception as e:
+            print(e)
+
+    def cap_image(self, display=False):
+        ret, img = self.cam.read()
+        # flip image
+        if self.flip:
+            img = cv2.flip(img, -1)
+
+        if not ret: 
+            raise ValueError("Cannot read frame")
+
+        if display:
+            cv2.imshow('Captured Image', img)
+            cv2.waitKey(0)
+        return img
+    def stream(self):
+        while (True):
+            success, frame = self.cam.read()
+            if success:  # frame read successfully
+                if self.flip:
+                    frame = cv2.flip(frame, -1)
+                cv2.imshow('show', frame)
+                k = cv2.waitKey(1)
+                if k == ord('q'):
+                    break
+
+
+if __name__=="__main__":
+    OUT_DIR = "../img_output/"
+    DISPLAY = True
+    current_time = datetime.datetime.now()
+    formatted_time = current_time.strftime('%Y_%m_%d_%H_%M_%S.%f')[:-7]
+    logdata_dir = f'{OUT_DIR}img_sample_{formatted_time}/'
+    os.makedirs(logdata_dir)
+    print("here")
+    cam = infinity_cam(0+cv2.CAP_DSHOW)
+    print("Now here")
+    cam.stream()
+    cam.cap_image(display=True)
+    img = cam.cap_image()
+    cv2.imwrite(f'{logdata_dir}img_5x_calibration_scale.png', img)
